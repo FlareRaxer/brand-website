66ab1df99bd7d8b018aee2836400460bd2b110cd
FlareRaxer <jonaskruse123@gmail.com>
2025-04-21 13:05:57 +0200

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from 'next/script'; // Import the Script component
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
@@ -32,6 +33,25 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Analytics Scripts */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-WNGCG8E8PE"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WNGCG8E8PE');
+            `,
+          }}
+        />
+        {/* End Google Analytics Scripts */}
+
         <Navbar />
         {children}
       </body>
